e2c5efdcfea42d78d51000b3b7beb40cfe824010
NektoUa <illyabarkov@gmail.com>
2021-10-09 14:27:10 +0300

    add function addElement

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-function count() {
-    let firstNum = +document.querySelector('.argOne').value;
-    let secondNum = +document.querySelector('.argTwo').value;
-
-    let math = document.querySelector('.math-operation').value;
-
-    let sum = '';
-    let outResult = document.getElementById('result');
-
-    if (math == '+') {
-        sum = firstNum + secondNum;
-    }
-    else if (math == '-') {
-        sum = firstNum - secondNum;
-    }
-    else if (math == '*') {
-        sum = firstNum * secondNum;
-    }
-    else {
-        sum = firstNum / secondNum;
-    }
-
-    outResult.innerHTML = sum;
-
-    firstNum = '';
-
+function addElement() {
+    let boardItem = document.querySelectorAll('.block-keyboard_item');
+    let keyAtribute = '';
+    boardItem.forEach(function (elem) {
+        keyAtribute = elem.getAttribute('data-keyboard');
+        console.log(event.keyCode);
+        if (event.keyCode == keyAtribute) {
+            elem.classList.add('active');
+            setTimeout(function () { elem.classList.remove('active') }, 500);
+        }
+    })
 }
 
-document.querySelector('#equal').onclick = count;
+document.querySelector('.input-result').onkeyup = function (event) {
+    addElement();
+}
+
+
+// function count() {
+//     let firstNum = +document.querySelector('.argOne').value;
+//     let secondNum = +document.querySelector('.argTwo').value;
+
+//     let math = document.querySelector('.math-operation').value;
+
+//     let sum = '';
+//     let outResult = document.getElementById('result');
+
+//     if (math == '+') {
+//         sum = firstNum + secondNum;
+//     }
+//     else if (math == '-') {
+//         sum = firstNum - secondNum;
+//     }
+//     else if (math == '*') {
+//         sum = firstNum * secondNum;
+//     }
+//     else {
+//         sum = firstNum / secondNum;
+//     }
+
+//     outResult.innerHTML = sum;
+
+//     firstNum = '';
+
+// }
+
+// document.querySelector('#equal').onclick = count;
 
 
 
