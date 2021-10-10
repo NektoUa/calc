33f2e095f877c44bb21255f599b5dd1e204d0190
NektoUa <illyabarkov@gmail.com>
2021-10-10 21:09:08 +0300

    another

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+function mathOperation(val) {
+
+    console.log(val);
+}
+
 function addElement() {
+
     let boardItem = document.querySelectorAll('.block-keyboard_item');
     let keyAtribute = '';
+
     boardItem.forEach(function (elem) {
+        let numCode = 48;
         keyAtribute = elem.getAttribute('data-keyboard');
-        console.log(event.keyCode);
-        if (event.keyCode == keyAtribute) {
+        // console.log(event);
+        if (event.keyCode == keyAtribute || (event.keyCode - numCode) == keyAtribute) {
             elem.classList.add('active');
             setTimeout(function () { elem.classList.remove('active') }, 500);
         }
@@ -15,6 +23,11 @@ document.querySelector('.input-result').onkeyup = function (event) {
     addElement();
 }
 
+let val = document.querySelector('.input-result').value;
+
+document.querySelector('#equal').onkeyup = function (event) {
+    mathOperation(val)
+}
 
 // function count() {
 //     let firstNum = +document.querySelector('.argOne').value;
