8337062dfbb197e50c1665eae11628ad0aef8d18
NektoUa <illyabarkov@gmail.com>
2021-10-22 13:39:12 +0300

    add function deposit

--- main.js
+++ main.js
@@ -1,90 +1,19 @@
 "use strict";
 
+let arrayScreen = []
 
-
-let inputResult = document.querySelector('.input-result');
-
-let nums = document.querySelector('.nums');
-let equal = document.querySelector('#equal');
-let clear = document.querySelector('#clear');
-
-let arrayOperation = ['+', '-', '*', '/'];
-
-function active() {
-    this.classList.toggle('active');
-}
-
-clear.onmousedown = active;
-clear.onmouseup = active;
-
-
-
-function mathOperation() {
-    let val = document.querySelector('.input-result').split('');
-    let num;
-    for (let i = 0; i < val.length; i++) {
-        if (typeof +val[i] == 'number') {
-            console.log(val)
-        }
-        num += val[i]
-
-    }
+function deposit(event) {
+    let out = document.querySelector('.screen');
+    let information = event.srcElement.textContent
+    out.innerHTML = information;
+    arrayScreen.push(information);
+    console.log(event);
+    console.log(arrayScreen);
 
 }
 
-function addElement() {
+document.querySelector('#seven').onclick = deposit;
 
-    let boardItem = document.querySelectorAll('.block-keyboard_item');
-    let keyAtribute = '';
-
-    boardItem.forEach(function (elem) {
-        let numCode = 48;
-        keyAtribute = elem.getAttribute('data-keyboard');
-        // console.log(event);
-        if (event.keyCode == keyAtribute || (event.keyCode - numCode) == keyAtribute) {
-            elem.classList.add('active');
-            setTimeout(function () { elem.classList.remove('active') }, 500);
-        }
-    })
-}
-
-document.querySelector('.input-result').onkeyup = function (event) {
-    addElement();
-}
-
-document.querySelector('#equal').onclick = () => {
-    mathOperation()
-}
-
-// function count() {
-//     let firstNum = +document.querySelector('.argOne').value;
-//     let secondNum = +document.querySelector('.argTwo').value;
-
-//     let math = document.querySelector('.math-operation').value;
-
-//     let sum = '';
-//     let outResult = document.getElementById('result');
-
-//     if (math == '+') {
-//         sum = firstNum + secondNum;
-//     }
-//     else if (math == '-') {
-//         sum = firstNum - secondNum;
-//     }
-//     else if (math == '*') {
-//         sum = firstNum * secondNum;
-//     }
-//     else {
-//         sum = firstNum / secondNum;
-//     }
-
-//     outResult.innerHTML = sum;
-
-//     firstNum = '';
-
-// }
-
-// document.querySelector('#equal').onclick = count;
 
 
 
